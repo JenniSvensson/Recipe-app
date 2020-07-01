c9f57b341e190823515b617fa72506949315eead
Jenni <jenni.svensson@live.se>
2020-07-01 21:47:50 +0200

    added reducer that takes care of the dispatched action

--- src/store/recipe/actions.js
+++ src/store/recipe/actions.js
@@ -1,11 +1,19 @@
 import { apiUrl } from "../../config/constants";
 import axios from "axios";
 
+export function fetchedRecipes(data) {
+  return {
+    type: "FETCH_RECIPES",
+    payload: data,
+  };
+}
+
 export async function getRecipes(dispatch, getState) {
   try {
     const response = await axios.get(`${apiUrl}/recipe`);
 
     console.log("got the data", response.data);
+    dispatch(fetchedRecipes(response.data));
   } catch (error) {
     console.log("error", error);
   }
---
+++ src/store/recipe/reducer.js
@@ -0,0 +1,18 @@
+const initialState = {
+  recipes: [],
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case "FETCH_RECIPES": {
+      return {
+        ...state.recipes,
+        ...action.payload,
+      };
+    }
+
+    default: {
+      return state;
+    }
+  }
+};
