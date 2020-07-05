001ca8ccf649261dcb38b2a65634052307b56dbe
Jenni <jenni.svensson@live.se>
2020-07-05 14:59:24 +0200

    added selector to select a specific recipe based on the routeparams id

--- src/pages/RecipeDetails/index.js
+++ src/pages/RecipeDetails/index.js
@@ -2,15 +2,17 @@ import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { getRecipes } from "../../store/recipe/actions";
-import { selectRecipes } from "../../store/recipe/selectors";
+import { selectRecipes, selectRecipeById } from "../../store/recipe/selectors";
 export default function RecipeDetails() {
   const params = useParams();
   const RecipeId = parseInt(params.id);
   const dispatch = useDispatch();
   const Recipes = useSelector(selectRecipes);
+  const thisRecipe = useSelector(selectRecipeById(RecipeId));
+  console.log(thisRecipe);
 
   useEffect(() => {
-    //checks if there is no recipes or ingredients if so it will go and fetch them
+    //checks if there is no recipes if so it will go and fetch them
     if (Recipes) {
       dispatch(getRecipes);
     }
