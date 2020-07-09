960863fa55e9c42241ace9f15faeb6a416646107
Jenni <jenni.svensson@live.se>
2020-07-09 20:12:09 +0200

    Fixed some paths

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -21,8 +21,8 @@ export default function Navigation() {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav style={{ width: "100%" }} fill>
-          <NavbarItem path="/" linkText="Home" />
-          <NavbarItem path="/recipefinder" linkText="Recipe Finder" />
+          <NavbarItem path="/" linkText="Recipe Finder" />
+          <NavbarItem path="/Discover" linkText="Discover" />
           {loginLogoutControls}
         </Nav>
       </Navbar.Collapse>
