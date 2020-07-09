e1e4b86a2a7a520869fd84feb6a376ed7268ed9b
Jenni <jenni.svensson@live.se>
2020-07-09 20:47:20 +0200

    added styling to recipes discovery page

--- src/pages/RecipeFinder/index.js
+++ src/pages/RecipeFinder/index.js
@@ -181,41 +181,44 @@ export default function RecipeFinder() {
         </form>
       }
       <br></br>
-      {
-        <Container>
-          <Row>
-            {recipes ? (
-              recipes.map((recipe) => {
-                return (
-                  <Col xs={3} className="mb-5 " key={recipe.id}>
-                    <Card className="h-100 shadow-sm bg-white rounded">
-                      <Card.Img variant="top" src={`${recipe.imageUrl}`} />
-                      <Card.Body className="d-flex flex-column">
-                        <div className="d-flex mb-2 justify-content-between ">
-                          <Card.Title className="mb-0 font-weight-bold">
-                            {recipe.name}
-                          </Card.Title>
-                        </div>
 
-                        <Card.Text>
-                          Cooking time: {recipe.preperationTime} Min
-                        </Card.Text>
-                        <Card.Link>
-                          <a href={`/Recipes/${recipe.id}`}>
-                            Go to the instructions
-                          </a>
-                        </Card.Link>
-                      </Card.Body>
-                    </Card>
-                  </Col>
-                );
-              })
-            ) : (
-              <p></p>
-            )}
-          </Row>
-        </Container>
-      }
+      <Container>
+        <Row>
+          {recipes ? (
+            recipes.map((recipe) => {
+              return (
+                <Col xs={3} className="mb-5 " key={recipe.id}>
+                  <Card className="h-100 shadow-sm bg-white rounded">
+                    <Card.Img variant="top" src={`${recipe.imageUrl}`} />
+                    <Card.Body className="d-flex flex-column">
+                      <div className="d-flex mb-2 justify-content-between ">
+                        <Card.Title className="mb-0 font-weight-bold">
+                          {recipe.name}
+                        </Card.Title>
+                      </div>
+
+                      <Card.Text>
+                        Cooking time: {recipe.preperationTime} min
+                        <br></br>
+                        Flavourprofile: {recipe.flavourProfile}
+                        <br></br>
+                        Dish type: {recipe.dishType}
+                      </Card.Text>
+                      <Card.Link>
+                        <a href={`/Recipes/${recipe.id}`}>
+                          Go to the instructions
+                        </a>
+                      </Card.Link>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              );
+            })
+          ) : (
+            <p></p>
+          )}
+        </Row>
+      </Container>
     </div>
   );
 }
