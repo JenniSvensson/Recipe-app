fa666d9b210f9c823c87f53529712c25478680ec
Jenni <jenni.svensson@live.se>
2020-07-09 20:00:23 +0200

    added some css and bootstrap to the recipeFinder page

--- src/pages/RecipeFinder/index.js
+++ src/pages/RecipeFinder/index.js
@@ -5,16 +5,27 @@ import {
   selectfilteredRecipes,
   selectfilteredIngredients,
 } from "../../store/recipe/selectors";
-import { selectIngredients } from "../../store/ingredients/selector";
+// import { selectIngredients } from "../../store/ingredients/selector";
 import { getRecipes, getDietRecipes } from "../../store/recipe/actions";
 import { getIngredients } from "../../store/ingredients/actions";
-
+import {
+  Form,
+  Button,
+  Spinner,
+  Container,
+  Image,
+  Row,
+  Card,
+  Badge,
+  Col,
+} from "react-bootstrap";
+import "./recipeFinder.css";
 export default function RecipeFinder() {
   const [recipes, setRecipes] = useState();
   const [input, setInput] = useState({
     ingredient: "",
-    flavourProfile: "",
-    dishType: "",
+    flavourProfile: "sweet",
+    dishType: "breakfast",
     diet: "all",
   });
 
@@ -62,7 +73,6 @@ export default function RecipeFinder() {
   }
 
   function handleClick(e) {
-    console.log("what is this", input);
     filterRecipe();
     console.log(recipes);
   }
@@ -83,72 +93,129 @@ export default function RecipeFinder() {
   console.log(input);
   return (
     <div>
-      <h2>Please answer these questions to find something nice to cook.</h2>
-
       {
         <form>
-          <select onChange={handleChange} name="diet">
-            <option value="">All</option>
-            <option value="vegan">Vegan</option>
-            <option value="vegetarian">Vegetarian</option>
-            <option value="glutenFree">Gluten-free</option>
-          </select>
           {Ingredients.length ? (
             <div>
-              <select name="ingredient" type="text" onChange={handleChange}>
-                {Ingredients.map((ingredient) => {
-                  return (
-                    <option key={ingredient.id} value={ingredient.name}>
-                      {ingredient.name}
-                    </option>
-                  );
-                })}
-              </select>
+              <Container>
+                <h3>
+                  Please answer these questions to find something nice to cook.
+                </h3>
 
-              <input
-                name="flavourProfile"
-                type="text"
-                list="flavourProfile"
-                onChange={handleChange}
-              />
-              <datalist id="flavourProfile">
-                {Recipes.map((recipe) => {
-                  return (
-                    <option key={recipe.id} value={recipe.flavourProfile} />
-                  );
-                })}
-              </datalist>
-              <input
-                name="dishType"
-                type="text"
-                list="dishType"
-                onChange={handleChange}
-              />
-              <datalist id="dishType">
-                {Recipes.map((recipe) => {
-                  return <option key={recipe.id} value={recipe.dishType} />;
-                })}
-              </datalist>
-              <input type="button" value="Search" onClick={handleClick} />
+                <Form>
+                  <Form.Group>
+                    <p>I am going to eat </p>
+
+                    <Form.Group>
+                      <Form.Control
+                        onChange={handleChange}
+                        name="dishType"
+                        as="select"
+                      >
+                        <option value="Breakfast">Breakfast</option>
+                        <option value="Lunch">Lunch</option>
+                        <option value="Dinner">Dinner</option>
+                        <option value="Dessert">Dessert</option>
+                      </Form.Control>
+                    </Form.Group>
+                  </Form.Group>
+
+                  <p>I want it to be </p>
+                  <Form.Group>
+                    <Form.Control
+                      onChange={handleChange}
+                      name="diet"
+                      as="select"
+                    >
+                      <option value="">All</option>
+                      <option value="vegan">Vegan</option>
+                      <option value="vegetarian">Vegetarian</option>
+                      <option value="glutenFree">Gluten-free</option>
+                    </Form.Control>
+                  </Form.Group>
+
+                  <p>I feel like something</p>
+                  <Form.Group>
+                    <Form.Control
+                      onChange={handleChange}
+                      name="flavourProfile"
+                      as="select"
+                    >
+                      <option value="sweet">Sweet</option>
+                      <option value="salty">Salty</option>
+                      <option value="savoury">Savoury</option>
+                      <option value="spicy">Spicy</option>
+                    </Form.Control>
+                  </Form.Group>
+
+                  <Form.Group>
+                    <p>It has to contain</p>
+
+                    <input
+                      className="inputForm"
+                      name="ingredients"
+                      type="text"
+                      list="ingredients"
+                      onChange={handleChange}
+                    />
+                    <datalist id="ingredients">
+                      {Ingredients.map((ing) => {
+                        return <option key={ing.id} value={ing.name} />;
+                      })}
+                    </datalist>
+                  </Form.Group>
+                </Form>
+
+                <br></br>
+
+                <Button variant="primary" onClick={handleClick}>
+                  Search
+                </Button>
+              </Container>
             </div>
           ) : (
-            <p>Loading</p>
+            <Spinner animation="border" role="status">
+              <span className="sr-only">Loading...</span>
+            </Spinner>
           )}
         </form>
       }
-      {recipes ? (
-        recipes.map((recipe) => {
-          return (
-            <div key={recipe.id}>
-              <img src={`${recipe.imageUrl}`} />
-              <h3> {recipe.name}</h3>
-              <a href={`/Recipes/${recipe.id}`}>See more details</a>
-            </div>
-          );
-        })
-      ) : (
-        <p></p>
-      )}
+      <br></br>
+      {
+        <Container>
+          <Row>
+            {recipes ? (
+              recipes.map((recipe) => {
+                return (
+                  <Col xs={3} className="mb-5 " key={recipe.id}>
+                    <Card className="h-100 shadow-sm bg-white rounded">
+                      <Card.Img variant="top" src={`${recipe.imageUrl}`} />
+                      <Card.Body className="d-flex flex-column">
+                        <div className="d-flex mb-2 justify-content-between ">
+                          <Card.Title className="mb-0 font-weight-bold">
+                            {recipe.name}
+                          </Card.Title>
+                        </div>
+
+                        <Card.Text>
+                          Cooking time: {recipe.preperationTime} Min
+                        </Card.Text>
+                        <Card.Link>
+                          <a href={`/Recipes/${recipe.id}`}>
+                            Go to the instructions
+                          </a>
+                        </Card.Link>
+                      </Card.Body>
+                    </Card>
+                  </Col>
+                );
+              })
+            ) : (
+              <p></p>
+            )}
+          </Row>
+        </Container>
+      }
     </div>
   );
 }
