03e9cd786fc641d34b1ee9636ce03cda5e1a30d2
Jenni <jenni.svensson@live.se>
2020-12-07 18:02:59 +0100

    Added bg and changed linktexts

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -14,15 +14,15 @@ export default function Navigation() {
   const loginLogoutControls = token ? <LoggedIn /> : <LoggedOut />;
 
   return (
-    <Navbar expand="lg">
+    <Navbar bg="secondary" expand="lg">
       <Navbar.Brand className="RecipeFinder" as={NavLink} to="/">
         Recipe Finder
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav style={{ width: "100%" }} fill>
-          <NavbarItem path="/" linkText="Recipe Finder" />
-          <NavbarItem path="/allRecipes" linkText="Recipes" />
+          <NavbarItem path="/" linkText="Find recipes" />
+          <NavbarItem path="/allRecipes" linkText=" All recipes" />
           {loginLogoutControls}
         </Nav>
       </Navbar.Collapse>
